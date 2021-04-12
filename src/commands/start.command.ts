8e75886d4e2f8d70d4905a68b477c8d6a41bd699
brunosana <brunosa@dcomp.ufs.br>
2021-04-12 18:58:22 -0300

    fix: Parse and get dates of the start and list commands

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -1,7 +1,7 @@
 import Discord from 'discord.js';
+
 import { Marathon, MarathonProps } from '../models/marathon';
 import { User } from '../models/user';
-import messageCodes from '../utils/messageCodes';
 import { helpObject as newHelp } from './new.command';
 
 interface sendMethodProp extends Discord.GuildChannel {
@@ -21,6 +21,7 @@ const execute = async (discord: Discord.Client, message: Discord.Message) =>{
         throw new Error(`Just the dealer of current opened marathon can invoke this command. This thime, the current Dealer is <@${dealer.userId}>`)
     }
     marathons[0].status = 'IN_PROGRESS';
+    marathons[0].date = new Date();
     await marathons[0].save();
     if(!message.guild){
         throw new Error("The channel can't be finded, please, contact Admin");
